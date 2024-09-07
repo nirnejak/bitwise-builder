0c3e17e0e15ea68eca1db3b1bd286ee2a7420686
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-08 04:13:53 +0530

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,59 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
+import Image from "next/image"
+import Link from "next/link"
+
+import CoverImage from "assets/team.jpg"
 import { generateMetadata } from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
   path: "/",
-  title: "Next.js App",
-  description: "Next.js Typescript Starter",
+  title: "Bitwise Builder",
+  description:
+    "Bitwise Builder, an agency specializing in Python and builds high performance web applications.",
 })
 
 const Home: React.FC = () => {
   return (
-    <main className="grid h-screen place-content-center">
-      <h1 className="text-center text-xl font-medium text-zinc-300">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+    <main className="">
+      <section className="container mb-24 mt-48">
+        <div className="mb-8">
+          <p className="inline rounded-full border bg-zinc-100 px-3.5 py-1.5 text-xs text-zinc-700">
+            Open to projects
+          </p>
+        </div>
+        <h1 className="mb-12 text-8xl font-semibold tracking-tighter text-zinc-900">
+          A single place for <br />
+          all your dev work.
+        </h1>
+        <div className="flex items-center justify-between">
+          <p className="max-w-[670px] text-lg text-zinc-700">
+            Building high-performance, scalable web apps with Python, Django,
+            React, and Next.js. Empowering businesses with seamless user
+            experiences and innovative digital solutions.
+          </p>
+          <div>
+            <Link
+              href={"/contact-us/"}
+              className="rounded-full bg-zinc-900 px-10 py-4 text-zinc-50"
+            >
+              Contact us
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className="px-5">
+        <Image
+          src={CoverImage}
+          alt="Team working together"
+          className="w-full rounded-3xl"
+          quality={100}
+          priority
+          width={1538.5}
+          height={1054}
+        />
+      </section>
     </main>
   )
 }
