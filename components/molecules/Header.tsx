7276f918dda3d0eb12bbe289ff79a803fece1eba
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-08 23:47:33 +0530

    Header: as fixed

--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -5,49 +5,49 @@ import Link from "next/link"
 
 const Header: React.FC = () => {
   return (
-    <header className="container flex items-center justify-between py-8">
-      <div className="flex items-center gap-20">
-        <Link
-          href={"/"}
-          className="text-xl font-semibold leading-none text-zinc-900"
-        >
-          Bitwise Builder
-        </Link>
-        <nav className="flex items-center gap-6 font-medium leading-none text-zinc-500">
+    <header className="fixed top-0 w-full py-8">
+      <div className="container flex items-center justify-between">
+        <div className="flex items-center gap-20">
           <Link
-            href={"/services/"}
-            className="transition-colors hover:text-zinc-900"
+            href={"/"}
+            className="text-xl font-semibold leading-none text-zinc-900"
           >
-            Services
+            Bitwise Builder
           </Link>
-          <Link
-            href={"/projects/"}
-            className="transition-colors hover:text-zinc-900"
-          >
-            Projects
-          </Link>
-          <Link
-            href={"/blogs/"}
-            className="transition-colors hover:text-zinc-900"
-          >
-            Blogs
-          </Link>
-          <Link
-            href={"/about/"}
-            className="transition-colors hover:text-zinc-900"
-          >
-            About
-          </Link>
-          <Link
-            href={"/careers/"}
-            className="flex items-center gap-1.5 transition-colors hover:text-zinc-900"
-          >
-            Careers
-            <CircleFill size={5} className="text-pink-600" />
-          </Link>
-        </nav>
-      </div>
-      <div>
+          <nav className="flex items-center gap-6 font-medium leading-none text-zinc-500">
+            <Link
+              href={"/services/"}
+              className="transition-colors hover:text-zinc-900"
+            >
+              Services
+            </Link>
+            <Link
+              href={"/projects/"}
+              className="transition-colors hover:text-zinc-900"
+            >
+              Projects
+            </Link>
+            <Link
+              href={"/blogs/"}
+              className="transition-colors hover:text-zinc-900"
+            >
+              Blogs
+            </Link>
+            <Link
+              href={"/about/"}
+              className="transition-colors hover:text-zinc-900"
+            >
+              About
+            </Link>
+            <Link
+              href={"/careers/"}
+              className="flex items-center gap-1.5 transition-colors hover:text-zinc-900"
+            >
+              Careers
+              <CircleFill size={5} className="text-pink-600" />
+            </Link>
+          </nav>
+        </div>
         <Link
           href={"/contact-us/"}
           className="rounded-full bg-zinc-200 px-5 py-2.5 text-sm text-zinc-700 transition-colors hover:bg-zinc-300"
