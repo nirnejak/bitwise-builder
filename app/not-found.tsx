e2f4499237fc5dc1299265e7ad1a2c0f823f35e5
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-09 04:49:59 +0530

    Not Found: copy updated, home button added, metadata updated

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,23 +2,33 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
+import Link from "next/link"
+
 import { generateMetadata } from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
   path: "/",
-  title: "Bitwise Builder",
+  title: "Not Found | Bitwise Builder",
   description:
-    "Bitwise Builder, an development agency specializing in Python and Django, we and build high performance web applications.",
+    "Can't find the page you're looking for on the Bitwise Builder website.",
 })
 
-const Home: React.FC = () => {
+const NotFoundPage: React.FC = () => {
   return (
-    <main className="grid h-screen place-content-center">
-      <h1 className="text-8xl font-semibold tracking-tighter text-zinc-900">
-        404
+    <main className="grid h-screen place-content-center text-center">
+      <h1 className="mb-12 text-6xl font-semibold tracking-tighter text-zinc-900">
+        Seems like you{"'"}re lost.
       </h1>
+      <div>
+        <Link
+          href={"/"}
+          className="rounded-full bg-zinc-900 px-5 py-2.5 text-sm font-medium text-zinc-50 transition-colors hover:bg-zinc-800"
+        >
+          Go to Home
+        </Link>
+      </div>
     </main>
   )
 }
 
-export default Home
+export default NotFoundPage
