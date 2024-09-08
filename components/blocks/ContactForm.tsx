a08a2283902e71f2c59780bf80155a95ad005ba2
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-09 04:38:27 +0530

    Contact: style updated

--- components/blocks/ContactForm.tsx
+++ components/blocks/ContactForm.tsx
@@ -1,7 +1,6 @@
 "use client"
 import * as React from "react"
 
-import { CircleFill } from "akar-icons"
 import { motion } from "framer-motion"
 
 import { BASE_TRANSITION } from "utils/animation"
@@ -19,17 +18,6 @@ const ContactForm: React.FC = () => {
 
   return (
     <section className="container mb-24 mt-40">
-      <div className="flex">
-        <motion.span
-          initial={{ translateY: 10, opacity: 0 }}
-          animate={{ translateY: 0, opacity: 1 }}
-          transition={{ delay: 0, ...BASE_TRANSITION }}
-          className="flex items-center gap-1.5 rounded-full bg-pink-100 px-3.5 py-1.5 text-xs text-pink-700"
-        >
-          <CircleFill size={5} className="text-pink-600" />
-          Open to projects
-        </motion.span>
-      </div>
       <motion.h1
         initial={{ translateY: 10, opacity: 0 }}
         animate={{ translateY: 0, opacity: 1 }}
@@ -47,7 +35,7 @@ const ContactForm: React.FC = () => {
           initial={{ translateY: 10, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ delay: 0.2, ...BASE_TRANSITION }}
-          className="rounded-2xl bg-zinc-200 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
+          className="rounded-2xl bg-zinc-100 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
           type="text"
           placeholder="Name"
           name="name"
@@ -60,7 +48,7 @@ const ContactForm: React.FC = () => {
           initial={{ translateY: 10, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ delay: 0.25, ...BASE_TRANSITION }}
-          className="rounded-2xl bg-zinc-200 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
+          className="rounded-2xl bg-zinc-100 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
           type="email"
           placeholder="Email"
           name="email"
@@ -73,7 +61,7 @@ const ContactForm: React.FC = () => {
           initial={{ translateY: 10, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ delay: 0.3, ...BASE_TRANSITION }}
-          className="rounded-2xl bg-zinc-200 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
+          className="rounded-2xl bg-zinc-100 px-6 py-5 text-lg leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
           placeholder="Message"
           rows={4}
           name="message"
@@ -87,7 +75,7 @@ const ContactForm: React.FC = () => {
           initial={{ translateY: 10, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ delay: 0.35, ...BASE_TRANSITION }}
-          className="mt-4 rounded-2xl bg-zinc-900 py-5 text-zinc-50 focus:outline-none"
+          className="mt-4 rounded-2xl bg-zinc-900 py-5 text-zinc-50 hover:bg-zinc-800 focus:bg-zinc-800 focus:outline-none"
         >
           Contact us
         </motion.button>
