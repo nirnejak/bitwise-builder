75372f0433f40c3737fabe9c042b6a46d83a2a95
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-08 22:24:15 +0530

    Hero reveal animation added, home divided into blocks

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import * as React from "react"
 
 import { type Metadata } from "next"
 
-import { ArrowUpRight, CircleFill } from "akar-icons"
-import Image from "next/image"
-import Link from "next/link"
-
-import CoverImage from "assets/team.jpg"
+import HomeHero from "@/components/blocks/HomeHero"
+import Services from "@/components/blocks/Services"
 import { generateMetadata } from "utils/seo"
 
 export const metadata: Metadata = generateMetadata({
@@ -19,99 +16,8 @@ export const metadata: Metadata = generateMetadata({
 const Home: React.FC = () => {
   return (
     <main className="">
-      <section className="container mb-24 mt-40">
-        <div className="flex">
-          <span className="flex items-center gap-1.5 rounded-full bg-pink-100 px-3.5 py-1.5 text-xs text-pink-700">
-            <CircleFill size={5} className="text-pink-500" />
-            Open to projects
-          </span>
-        </div>
-        <h1 className="mb-12 mt-9 text-8xl font-semibold tracking-tighter text-zinc-900">
-          A single place for <br />
-          all your dev work.
-        </h1>
-        <div className="flex items-center justify-between">
-          <p className="max-w-[670px] text-lg text-zinc-600">
-            Building high-performance, scalable web apps with Python, Django,
-            React, and Next.js. Empowering businesses with seamless user
-            experiences and innovative digital solutions.
-          </p>
-          <div>
-            <Link
-              href={"/contact-us/"}
-              className="rounded-full bg-zinc-900 px-10 py-4 text-zinc-50"
-            >
-              Contact us
-            </Link>
-          </div>
-        </div>
-      </section>
-      <section className="mb-48 px-5">
-        <Image
-          src={CoverImage}
-          alt="Team working together"
-          className="w-full rounded-3xl"
-          quality={100}
-          priority
-          width={1538.5}
-          height={1054}
-        />
-      </section>
-      <section className="container mb-48 flex">
-        <div className="flex-1">
-          <h2 className="mb-12 mt-9 text-7xl font-semibold tracking-tighter text-zinc-900">
-            What can we
-            <br />
-            do for you?
-          </h2>
-        </div>
-        <div className="grid flex-1 grid-cols-2 gap-8">
-          <Link
-            href={"/services/web-development/"}
-            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-          >
-            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-            </span>
-            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-              Web Development
-            </p>
-          </Link>
-          <Link
-            href={"/services/app-development/"}
-            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-          >
-            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-            </span>
-            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-              App Development
-            </p>
-          </Link>
-          <Link
-            href={"/services/cloud-devops/"}
-            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-          >
-            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-            </span>
-            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-              Cloud & DevOps
-            </p>
-          </Link>
-          <Link
-            href={"/services/product-design/"}
-            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-          >
-            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-            </span>
-            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-              Product Design
-            </p>
-          </Link>
-        </div>
-      </section>
+      <HomeHero />
+      <Services />
     </main>
   )
 }
---
+++ components/blocks/HomeHero.tsx
@@ -0,0 +1,79 @@
+"use client"
+import * as React from "react"
+
+import { CircleFill } from "akar-icons"
+import { motion } from "framer-motion"
+import Image from "next/image"
+import Link from "next/link"
+
+import CoverImage from "assets/team.jpg"
+import { BASE_TRANSITION } from "utils/animation"
+
+const HomeHero: React.FC = () => {
+  return (
+    <>
+      <section className="container mb-24 mt-40">
+        <div className="flex">
+          <motion.span
+            initial={{ translateY: 10, opacity: 0 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            transition={{ delay: 0, ...BASE_TRANSITION }}
+            className="flex items-center gap-1.5 rounded-full bg-pink-100 px-3.5 py-1.5 text-xs text-pink-700"
+          >
+            <CircleFill size={5} className="text-pink-500" />
+            Open to projects
+          </motion.span>
+        </div>
+        <motion.h1
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0.1, ...BASE_TRANSITION }}
+          className="mb-12 mt-9 text-8xl font-semibold tracking-tighter text-zinc-900"
+        >
+          <span>
+            A single place for <br />
+          </span>
+          <span>all your dev work.</span>
+        </motion.h1>
+        <motion.div
+          initial={{ translateY: 10, opacity: 0 }}
+          animate={{ translateY: 0, opacity: 1 }}
+          transition={{ delay: 0.2, ...BASE_TRANSITION }}
+          className="flex items-center justify-between"
+        >
+          <p className="max-w-[670px] text-lg text-zinc-600">
+            Building high-performance, scalable web apps with Python, Django,
+            React, and Next.js. Empowering businesses with seamless user
+            experiences and innovative digital solutions.
+          </p>
+          <div>
+            <Link
+              href={"/contact-us/"}
+              className="rounded-full bg-zinc-900 px-10 py-4 text-zinc-50"
+            >
+              Contact us
+            </Link>
+          </div>
+        </motion.div>
+      </section>
+      <motion.section
+        initial={{ translateY: 10, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1 }}
+        transition={{ delay: 0.3, ...BASE_TRANSITION }}
+        className="mb-48 px-5"
+      >
+        <Image
+          src={CoverImage}
+          alt="Team working together"
+          className="w-full rounded-3xl"
+          quality={100}
+          priority
+          width={1538.5}
+          height={1054}
+        />
+      </motion.section>
+    </>
+  )
+}
+
+export default HomeHero
