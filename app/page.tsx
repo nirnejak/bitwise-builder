0408f9011503a60ec6452ec8137ed9f8c34a1e63
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-09-08 21:48:38 +0530

    Home: services section updated with title

--- app/page.tsx
+++ app/page.tsx
@@ -54,40 +54,60 @@ const Home: React.FC = () => {
           height={1054}
         />
       </section>
-      <section className="container mb-48 grid grid-cols-3 gap-8">
-        <Link
-          href={"/services/web-development/"}
-          className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-        >
-          <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-            <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-          </span>
-          <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-            Web Development
-          </p>
-        </Link>
-        <Link
-          href={"/services/app-development/"}
-          className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-        >
-          <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-            <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-          </span>
-          <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-            App Development
-          </p>
-        </Link>
-        <Link
-          href={"/services/product-design/"}
-          className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
-        >
-          <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
-            <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
-          </span>
-          <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
-            Product Design
-          </p>
-        </Link>
+      <section className="container mb-48 flex">
+        <div className="flex-1">
+          <h2 className="mb-12 mt-9 text-7xl font-semibold tracking-tighter text-zinc-900">
+            What can we
+            <br />
+            do for you?
+          </h2>
+        </div>
+        <div className="grid flex-1 grid-cols-2 gap-8">
+          <Link
+            href={"/services/web-development/"}
+            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
+          >
+            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
+              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
+            </span>
+            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
+              Website Dev
+            </p>
+          </Link>
+          <Link
+            href={"/services/app-development/"}
+            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
+          >
+            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
+              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
+            </span>
+            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
+              iOS Apps
+            </p>
+          </Link>
+          <Link
+            href={"/services/product-design/"}
+            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
+          >
+            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
+              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
+            </span>
+            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
+              Android Apps
+            </p>
+          </Link>
+          <Link
+            href={"/services/product-design/"}
+            className="group relative flex aspect-square cursor-pointer rounded-2xl border p-5 transition-colors hover:border-zinc-900 hover:bg-zinc-900"
+          >
+            <span className="absolute right-3 top-3 rounded-full bg-zinc-900 p-2.5 text-zinc-50">
+              <ArrowUpRight className="transition-transform group-hover:-translate-y-1 group-hover:translate-x-1" />
+            </span>
+            <p className="mt-auto text-2xl leading-none group-hover:text-zinc-50">
+              Product Design
+            </p>
+          </Link>
+        </div>
       </section>
     </main>
   )
