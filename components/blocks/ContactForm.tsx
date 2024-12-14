803435bead9a80c17eb7c5bafc691c5fdc2d7bfc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-12-14 12:14:29 +0530

    Contact Form: layout updated

--- components/blocks/ContactForm.tsx
+++ components/blocks/ContactForm.tsx
@@ -31,41 +31,43 @@ const ContactForm: React.FC = () => {
         onSubmit={handleSubmit}
         className="flex max-w-screen-sm flex-col gap-4"
       >
-        <motion.input
-          initial={{ translateY: 10, opacity: 0 }}
-          animate={{ translateY: 0, opacity: 1 }}
-          transition={{ delay: 0.2, ...BASE_TRANSITION }}
-          className="rounded-2xl bg-zinc-100 px-6 py-5 leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={state.name}
-          onChange={(e) => {
-            setState({ ...state, [e.target.name]: e.target.value })
-          }}
-          required
-        />
-        <motion.input
-          initial={{ translateY: 10, opacity: 0 }}
-          animate={{ translateY: 0, opacity: 1 }}
-          transition={{ delay: 0.25, ...BASE_TRANSITION }}
-          className="rounded-2xl bg-zinc-100 px-6 py-5 leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={state.email}
-          onChange={(e) => {
-            setState({ ...state, [e.target.name]: e.target.value })
-          }}
-          required
-        />
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          <motion.input
+            initial={{ translateY: 10, opacity: 0 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            transition={{ delay: 0.2, ...BASE_TRANSITION }}
+            className="rounded-2xl bg-zinc-100 px-6 py-5 leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={state.name}
+            onChange={(e) => {
+              setState({ ...state, [e.target.name]: e.target.value })
+            }}
+            required
+          />
+          <motion.input
+            initial={{ translateY: 10, opacity: 0 }}
+            animate={{ translateY: 0, opacity: 1 }}
+            transition={{ delay: 0.25, ...BASE_TRANSITION }}
+            className="rounded-2xl bg-zinc-100 px-6 py-5 leading-none text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={state.email}
+            onChange={(e) => {
+              setState({ ...state, [e.target.name]: e.target.value })
+            }}
+            required
+          />
+        </div>
         <motion.textarea
           initial={{ translateY: 10, opacity: 0 }}
           animate={{ translateY: 0, opacity: 1 }}
           transition={{ delay: 0.3, ...BASE_TRANSITION }}
           className="rounded-2xl bg-zinc-100 px-6 py-5 text-zinc-900 placeholder:text-zinc-400 focus:outline-none"
           placeholder="Message"
-          rows={4}
+          rows={7}
           name="message"
           value={state.message}
           onChange={(e) => {
